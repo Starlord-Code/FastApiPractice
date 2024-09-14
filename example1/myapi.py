638b84ec9ec10f86950996abcba5a3fe99498649
Harsha <hdangeti@qti.qualcomm.com>
2024-09-15 03:04:02 +0530

    added all CRUD methods of example1

--- example1/myapi.py
+++ example1/myapi.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,9 +15,19 @@ students = {
     1: {
         "name": "john",
         "age": 17,
-        "class": "year 12"
+        "year": "year 12"
     }
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
 
 @app.get("/")
 def index():
@@ -28,4 +40,45 @@ def get_student(student_id: int = Path(description="The ID of the student you wa
     return students[student_id]
 
 #query parameter with example
-#google.com/
+#google.com/get-name?name=john 
+
+@app.get("/get-by-name/{student_id}")
+def get_student(*,student_id: int, name: Optional[str] = None, test: int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not Found with name : "+name}
+
+# request body and post methods
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student exists"}
+    students[student_id] = student
+    return students[student_id] 
+
+#puts methods 
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student doesn't present"}
+    if student.name != None:
+        students[student_id].name = student.name 
+    
+    if student.age != None:
+        students[student_id].age = student.age
+
+    if student.year != None:
+        students[student_id].year = student.year
+    return students[student_id]
+
+
+#delete method
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student doesn't exists"}
+    del students[student_id]
+
+    return {"Message": "Student deleted successfully"}
